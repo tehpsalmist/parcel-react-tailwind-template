6e0f92b6cb879b51fd09e0e08a760bc7f814c9e1
tehpsalmist <bstewardcodes@gmail.com>
2023-01-04 16:39:20 -0500

    use tailwind classes

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 
 export const App = () => {
-  return <main>Hello World!</main>
+  return <main className="h-screen flex-center">Hello World!</main>
 }
 
 const root = createRoot(document.getElementById('app')!)
